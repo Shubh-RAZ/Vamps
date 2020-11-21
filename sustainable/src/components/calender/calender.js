d70a60a5702526c6ab88deb158980842833b6b72
Shubham Raj <ce19b030@iittp.ac.in>
2020-11-21 15:54:51 +0530

    first commit

---
+++ sustainable/src/components/calender/calender.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import './calender.css'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+
+export default class calender extends Component {
+    render() {
+        return (
+            <div>
+                    <div className="wrapper">
+      <div className="calendar">
+        <div className="month">
+               <div className="prev" onclick="moveDate('prev')">
+                   <span>&#10094;</span>
+               </div>
+               <div>
+                   <h2 id="month"></h2>
+                   <p id="date_str"></p>
+               </div>
+               <div className="next" onclick="moveDate('next')">
+                   <span>&#10095;</span>
+               </div>
+           </div>
+           <div className="weekdays">
+               <div>Sun</div>
+               <div>Mon</div>
+               <div>Tue</div>
+               <div>Wed</div>
+               <div>Thu</div>
+               <div>Fri</div>
+               <div>Sat</div>
+           </div>
+           <div className="days">
+
+           </div>
+
+      </div>
+
+    </div>
+    <script type="text/javascript" src="calendarjava.js">
+
+        </script> 
+
+            </div>
+        )
+    }
+}
